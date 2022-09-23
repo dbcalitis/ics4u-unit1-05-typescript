8f43de1c0c9ea8fe6f5a74988f3870df599e4817
dbcalitis <daria.bernice.calitis@mths.ca>
2022-09-23 14:02:22 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+/**
+ * The program calculates the length
+ * of the wood that is 1 board foot in dimension
+ *
+ * By:      Daria Bernice Calitis
+ * Version: 1.0
+ * Since:   2022-09-19
+ */
+
+import promptSync from 'prompt-sync'
+
+const prompt = promptSync()
+
+// Constants
+const BOARD_FOOT = 144
+
+/**
+ * @param {number} width Width of the wood
+ * @param {number} height Height of the wood
+ * @returns {number} length Length of the wood
+ */
+function BoardLength (width: number, height: number): number {
+  const length = BOARD_FOOT / (width * height)
+  return length
+}
+
+console.log(
+  'This program figures out the length of a wooden board that is 1 board foot in dimension. (1 board foot is 144 inches³ of wood)'
+)
+
+// Gets the width of the wood
+const userWidth = prompt('Enter the width (inch): ')
+const userWidthFloat = parseFloat(userWidth)
+
+// Checks if the input width is a number
+if (isNaN(userWidthFloat)) {
+  console.log('Invalid Input.')
+} else {
+  // Gets the length of the wood
+  const userHeight = prompt('Enter the height (inch): ')
+  const userHeightFloat = parseFloat(userHeight)
+
+  // Checks if the input height is a number
+  if (isNaN(userHeightFloat)) {
+    console.log('Invalid Input.')
+  } else {
+    console.log(
+      `\nThe wood should be ${BoardLength(
+        userWidthFloat,
+        userHeightFloat
+      )} inch(es) long.`
+    )
+  }
+}
